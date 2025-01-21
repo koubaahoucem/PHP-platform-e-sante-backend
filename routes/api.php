3024fffc9342bdaf7d8d4636f92cb37d78fcc305
koubaahoucem <houssem.koubaa@iit.ens.tn>
2025-01-21 17:39:48 +0100

    authentification

--- routes/api.php
+++ routes/api.php
@@ -11,18 +11,30 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::middleware('api')->group(function () {
+Route::group(['middleware' => ['auth:api']], function () {
     Route::resource('pharmaciens', PharmacienController::class);
 });
 
-Route::middleware('api')->group(function () {
+Route::group(['middleware' => ['auth:api']], function () {
     Route::resource('patients', PatientController::class);
 });
 
-Route::middleware('api')->group(function () {
+Route::group(['middleware' => ['auth:api']], function () {
     Route::resource('ordonnances', OrdonnnaceController::class);
 });
 
-Route::middleware('api')->group(function () {
+Route::group(['middleware' => ['auth:api']], function () {
     Route::resource('medicaments', MedicamemtController::class);
 });
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'users'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refreshToken', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+});
+Route::get('users/verify-email', [AuthController::class, 'verifyEmail'])->name('verify.email');
