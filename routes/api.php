486ae9a79c2b321c2bc19749b515a001b92a754f
koubaahoucem <houssem.koubaa@iit.ens.tn>
2025-01-21 15:16:24 +0100

    ap

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\MedicamemtController;
+use App\Http\Controllers\OrdonnnaceController;
+use App\Http\Controllers\PatientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PharmacienController;
@@ -10,4 +13,16 @@ Route::get('/user', function (Request $request) {
 
 Route::middleware('api')->group(function () {
     Route::resource('pharmaciens', PharmacienController::class);
+});
+
+Route::middleware('api')->group(function () {
+    Route::resource('patients', PatientController::class);
+});
+
+Route::middleware('api')->group(function () {
+    Route::resource('ordonnances', OrdonnnaceController::class);
+});
+
+Route::middleware('api')->group(function () {
+    Route::resource('medicaments', MedicamemtController::class);
 });
